ad141e66279d4ed2da1845d1dce0db31a735a317
Christopher Bogs <cbogs@upenn.edu>
2023-12-10 00:32:58 -0500

    Starting code

---
+++ script.js
@@ -0,0 +1,33 @@
+//Tic-Tac-Toe by Christopher Bogs
+//Odin Project Intermediate Javascript
+
+
+function Gameboard() {
+
+    const board = [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+    ];  
+    
+    //what if we made x=2 and o=3, and blanks 0?  multiplying across gives 0, a multiple of 2, or of 3?
+
+    //get the board for the UI
+    const getBoard = () => board;
+
+    //place a marker
+    const placeMarker = (player, column, row) => {
+
+    }
+
+}
+
+
+function players() {
+
+}
+
+
+function game() {
+
+}
